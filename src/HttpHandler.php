52f0b0a61136ed68a3763892f3dd29b91eebd6d6
Dolgiy Yuriy <y.dolgiy@etpgpb.ru>
2020-11-01 14:35:06 +0300

    +HttpHandler

---
+++ src/HttpHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Parser\Handler;
+
+use Iset\App\HttpHandler;
+use Psr\Http\Message\ResponseInterface;
+
+class HttpHandler
+{
+
+  /**
+   * @var ResponseInterface
+   */
+  protected $response;
+
+  /**
+   *
+   * @param ResponseInterface $response
+   */
+  public function __construct(ResponseInterface $response) {
+    $this->response = $response;
+  }
+
+  /**
+   *
+   *
+   * @throws \RuntimeException
+   * @throws \InvalidArgumentException
+   */
+  public function __invoke(): ResponseInterface
+  {
+    return $this->response;
+  }
+
+  public function process($request, $response)
+  {
+    return $response;
+  }
+}
